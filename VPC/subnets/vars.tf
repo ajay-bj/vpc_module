b35bbe962e4d3bad2a2e70810182b5233021d4aa
CAEXT165 <ajay.ravi@go-yubi.com>
2025-10-15 12:27:02 +0530

    new

---
+++ VPC/subnets/vars.tf
@@ -0,0 +1,8 @@
+variable "vpc_id" {}
+variable "subnets" {}
+variable "azs" {}
+variable "env" {}
+variable "project_name" {}
+variable "name" {}
+variable "nacl_inbound" {}
+variable "nacl_outbound" {}
